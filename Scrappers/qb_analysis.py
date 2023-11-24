c263390704699ed5be612c2cc4d8e3a1c4a2cd48
Terence Lin <tlin41@dons.usfca.edu>
2023-11-23 21:04:17 -0800

    created json writer

--- Scrappers/qb_analysis.py
+++ Scrappers/qb_analysis.py
@@ -3,6 +3,7 @@ import collections
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+import json
 
 # def scrape_site(link,id):
 #     service = Service(ChromeDriverManager().install())
@@ -16,24 +17,29 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def process_text(text):
     lines = text.split('\n')
-    full_data = []
-    stats = lines[0]
     player_dict = collections.defaultdict(dict)
     lines.pop(0)
 
     for l in lines:
-        row = l.split(" ")
+        row = l.split(" ")[0:31]
         row[1:3] = [" ".join(row[1:3])]
-        print(row)
-        if len(row) == 31:
-            for i in range(len(stats) - 2):
-                if i != 1 and row[0].isnumeric():
-                    player_dict[row[1]][stats[i]] = row[i]
+        if row[1] == "Joshua Dobbs":
+            row.insert(4,"QB")
 
-    return
+        if row[1] == "Gardner Minshew":
+            row[1:3] =[" ".join(row[1:3])]
+        if row[0].isnumeric() and len(row) == 30 and row[1]!= "Taysom Hill":
+            player_dict[row[1]] ={"Rate":float(row[23]),"Tm":row[2],
+                                "Age":int(row[3]),
+                                "Cmp":int(row[8]), "Att":int(row[9]),
+                                "Cmp%":float(row[10]),"Yds":int(row[11]),
+                                "TD":int(row[12]),"TD%":float(row[13]),
+                                "Int":int(row[14]),"Int%":float(row[15]),"Sks":int(row[25])
+                                }
 
-def process_players(players):
-    player_dict = collections.defaultdict(dict)
+    return player_dict
+
+
 def main():
     service = Service(ChromeDriverManager().install())
     options = webdriver.ChromeOptions()
@@ -43,7 +49,10 @@ def main():
 
     table_id = "passing"
     table = driver.find_element("id",table_id)
-    process_text(table.text)
+    players = process_text(table.text)
+
+    with open("../JSON_data/quarterbacks.json","w") as outfile:
+        json.dump(players,outfile, indent = 2)
 
 
 main()
