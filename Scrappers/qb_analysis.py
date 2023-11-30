37dd70330f6f83785f6da3a0dd5f5b3b41859552
Terence Lin <tlin41@dons.usfca.edu>
2023-11-30 00:00:28 -0800

    refactored scraper

--- Scrappers/qb_analysis.py
+++ Scrappers/qb_analysis.py
@@ -1,58 +1,50 @@
 import collections
-
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from pymongo import MongoClient
 import json
-
-# def scrape_site(link,id):
-#     service = Service(ChromeDriverManager().install())
-#     options = webdriver.ChromeOptions()
-#     driver = webdriver.Chrome(service=service, options=options)
-#     driver.get(link)
-#     table = driver.find_element("id", id)
-#     return table.text
-#
+from webdriver_manager.chrome import ChromeDriverManager
 
 
-def process_text(text):
-    lines = text.split('\n')
+def get_qb(link, id):
+    service = Service(ChromeDriverManager().install())
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(service=service, options=options)
+    driver.get(link)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+    table = soup.find(id=id).find('tbody')
+
+    return table
+
+
+def process_text(table):
     player_dict = collections.defaultdict(dict)
-    lines.pop(0)
-
-    for l in lines:
-        row = l.split(" ")[0:31]
-        row[1:3] = [" ".join(row[1:3])]
-        if row[1] == "Joshua Dobbs":
-            row.insert(4,"QB")
-
-        if row[1] == "Gardner Minshew":
-            row[1:3] =[" ".join(row[1:3])]
-        if row[0].isnumeric() and len(row) == 30 and row[1]!= "Taysom Hill":
-            player_dict[row[1]] ={"Rate":float(row[23]),"Tm":row[2],
-                                "Age":int(row[3]),
-                                "Cmp":int(row[8]), "Att":int(row[9]),
-                                "Cmp%":float(row[10]),"Yds":int(row[11]),
-                                "TD":int(row[12]),"TD%":float(row[13]),
-                                "Int":int(row[14]),"Int%":float(row[15]),"Sks":int(row[25])
-                                }
+    rows_in_table = table.find_all('tr')
+    for row in rows_in_table:
+        th_elem = row.find_all('th')
+        td_elem = row.find_all('td')
+        for th in th_elem:
+            if th.text.isnumeric():
+                player_dict[td_elem[0].get_text()] = {}
+                for i in range(1, len(td_elem)):
+                    value = td_elem[i].get_text()
+                    if value.isdecimal():
+                        player_dict[td_elem[0].get_text()][td_elem[i].get('data-stat')] = float(value)
+                    elif value.isnumeric():
+                        player_dict[td_elem[0].get_text()][td_elem[i].get('data-stat')] = int(value)
+                    else:
+                        player_dict[td_elem[0].get_text()][td_elem[i].get('data-stat')] = value
 
     return player_dict
 
 
 def main():
-    service = Service(ChromeDriverManager().install())
-    options = webdriver.ChromeOptions()
-    driver = webdriver.Chrome(service=service, options=options)
-    url = "https://www.pro-football-reference.com/years/2023/passing.htm"
-    driver.get(url)
-
-    table_id = "passing"
-    table = driver.find_element("id",table_id)
-    players = process_text(table.text)
-
-    with open("../JSON_data/quarterbacks.json","w") as outfile:
-        json.dump(players,outfile, indent = 2)
+    table = get_qb("https://www.pro-football-reference.com/years/2023/passing.htm", "passing")
+    players = process_text(table)
+    print(players)
 
 
-main()
+if __name__ == "__main__":
+    main()
